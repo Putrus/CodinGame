747ac6ef40deb8ff35813db623cdcc56751a2d48
Putrus <puertaputra@gmail.com>
2022-05-12 01:43:56 +0200

    Map Pod Racing: Counting laps and remember all checkpoints after 1 lap

--- MadPodRacing/main.cpp
+++ MadPodRacing/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <algorithm>
 
 namespace math {
 
@@ -91,6 +92,10 @@ namespace math {
    bool operator==(const math::Vec2& lhs, const math::Vec2& rhs) {
       return lhs.x == rhs.x && lhs.y == rhs.y;
    }
+
+   bool operator!=(const math::Vec2& lhs, const math::Vec2& rhs) {
+      return lhs.x != rhs.x || lhs.y != rhs.y;
+   }
 }
 
 namespace game {
@@ -98,18 +103,34 @@ namespace game {
    private:
       //whether this checkpoint is next
       math::Vec2 m_position;
-      bool m_flag;
       int m_pod_dist;
       int m_pod_angle;
+      bool m_flag;
 
    public:
-      Checkpoint() : m_flag(false) {
+      Checkpoint() : m_pod_dist(0), m_pod_angle(0), m_flag(false) {
       }
 
-      Checkpoint(int x, int y, bool flag = false) : m_position(x, y), m_flag(false) {
+      Checkpoint(int x, int y, int pod_dist = 0, int pod_angle = 0, bool flag = false) : m_position(x, y), m_pod_dist(pod_dist), m_pod_angle(pod_angle), m_flag(false) {
       }
 
-      Checkpoint(math::Vec2 position, bool flag = false) : m_position(position), m_flag(false) {
+      Checkpoint(math::Vec2 position, int pod_dist = 0, int pod_angle = 0, bool flag = false) : Checkpoint(position.x, position.y, pod_dist, pod_angle, flag) {
+      }
+
+      void setPodDist(int pod_dist) {
+         m_pod_dist = pod_dist;
+      }
+
+      int getPodDist() const {
+         return m_pod_dist;
+      }
+
+      void setPodAngle(int pod_angle) {
+         m_pod_angle = pod_angle;
+      }
+
+      int getPodAngle() const {
+         return m_pod_angle;
       }
 
       void setFlag(bool flag) {
@@ -143,30 +164,54 @@ namespace game {
       return lhs.getPosition() == rhs.getPosition();
    }
 
-   class Pod : public math::Vec2 {
+   bool operator!=(const Checkpoint& lhs, const Checkpoint& rhs) {
+      return lhs.getPosition() != rhs.getPosition();
+   }
 
+   class Pod : public math::Vec2 {
    };
 
    class Game {
    private:
-      unsigned char lap = 1;
-      Pod pod;
-      Pod opponent;
-      std::vector<Checkpoint> checkpoints;
-      Checkpoint next_checkpoint;
+      
+
    public:
       //game loop
       void mainLoop() {
-
+         unsigned char lap = 0;
+         Pod pod;
+         Pod opponent;
+         std::vector<Checkpoint> checkpoints;
+         Checkpoint next_checkpoint;
+         Checkpoint last_checkpoint;
          while (true) {
             std::cin >> pod >> next_checkpoint >> opponent;
 
+            std::cerr << "Lap: " << (int)lap << std::endl;
+
+            auto next_checkpoint_it = std::find(checkpoints.begin(), checkpoints.end(), next_checkpoint);
+
+            //fill checkpoints vector
+            if (next_checkpoint_it == checkpoints.end()) {
+               checkpoints.push_back(next_checkpoint);
+            }
+
+            //increment lap
+            if (next_checkpoint == checkpoints[0] && next_checkpoint != last_checkpoint) {
+               ++lap;
+            }
+
+            for (int i = 0; i < checkpoints.size(); ++i) {
+               std::cerr << "Checkpoint " << i << ": " << checkpoints[i] << std::endl;
+            }
+
             std::cout << next_checkpoint << " " << "100" << std::endl;
+
+            last_checkpoint = next_checkpoint;
          }
       }
    };
 }
-
 
 int main()
 {
